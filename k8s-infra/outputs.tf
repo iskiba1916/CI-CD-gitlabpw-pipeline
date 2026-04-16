304c6bfc69da435eb6ba1d3f64519cf85ff5ff07
Skiba_Igor_Mikolaj <01187379@pw.edu.pl>
2026-04-16 18:04:14 +0200

    aktualizacja outputs.tf

---
+++ k8s-infra/outputs.tf
@@ -0,0 +1,26 @@
+output "master_ip" { 
+  value = openstack_networking_floatingip_v2.master_fip.address 
+} 
+
+output "worker_ips" { 
+  value = openstack_networking_floatingip_v2.worker_fip[*].address 
+}
+
+output "ansible_inventory" { 
+  value = <<-EOT
+[master]
+k8s-master ansible_host=${openstack_networking_floatingip_v2.master_fip.address} ansible_user=ubuntu
+
+[workers]
+%{ for i, ip in openstack_networking_floatingip_v2.worker_fip[*].address ~}
+worker-${i + 1} ansible_host=${ip} ansible_user=ubuntu
+%{ endfor ~}
+
+[k8s:children]
+master
+workers
+
+[k8s:vars]
+ansible_ssh_private_key_file=./moj_klucz.pem
+EOT
+}
